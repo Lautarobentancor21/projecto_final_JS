4e7234fca1a841f77fdf04f8b55040e5d8c489f9
Lautarobentancor <79489537+Lautarobentancor@users.noreply.github.com>
2021-08-07 15:35:31 -0300

    s

---
+++ pruebas.js
@@ -0,0 +1,11 @@
+$("#damevalor").click(function() {
+    algo()
+})
+
+function algo() {
+    let respuesta = $("#id")[0].innerHTML
+    if (respuesta == "gta 5") {
+        console.log("encontre el juego")
+
+    }
+}
